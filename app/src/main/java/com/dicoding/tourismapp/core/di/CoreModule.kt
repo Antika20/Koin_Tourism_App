59daed3fd41dc24523d1385fa3fed9255ffb8a64
Antika20 <antikaorin20@gmail.com>
2022-06-15 11:55:38 +0700

    Ini adalah latihan Dagger Tourism APP dicoding Expert

--- app/src/main/java/com/dicoding/tourismapp/core/di/CoreModule.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/di/CoreModule.kt
@@ -16,37 +16,37 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-val databaseModule = module {
-    factory { get<TourismDatabase>().tourismDao() }
-    single {
-        Room.databaseBuilder(
-            androidContext(),
-            TourismDatabase::class.java, "Tourism.db"
-        ).fallbackToDestructiveMigration().build()
-    }
-}
-
-val networkModule = module {
-    single {
-        OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-            .connectTimeout(120, TimeUnit.SECONDS)
-            .readTimeout(120, TimeUnit.SECONDS)
-            .build()
-    }
-    single {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://tourism-api.dicoding.dev/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(get())
-            .build()
-        retrofit.create(ApiService::class.java)
-    }
-}
-
-val repositoryModule = module {
-    single { LocalDataSource(get()) }
-    single { RemoteDataSource(get()) }
-    factory { AppExecutors() }
-    single<ITourismRepository> { TourismRepository(get(), get(), get()) }
-}
+//val databaseModule = module {
+//    factory { get<TourismDatabase>().tourismDao() }
+//    single {
+//        Room.databaseBuilder(
+//            androidContext(),
+//            TourismDatabase::class.java, "Tourism.db"
+//        ).fallbackToDestructiveMigration().build()
+//    }
+//}
+//
+//val networkModule = module {
+//    single {
+//        OkHttpClient.Builder()
+//            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+//            .connectTimeout(120, TimeUnit.SECONDS)
+//            .readTimeout(120, TimeUnit.SECONDS)
+//            .build()
+//    }
+//    single {
+//        val retrofit = Retrofit.Builder()
+//            .baseUrl("https://tourism-api.dicoding.dev/")
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .client(get())
+//            .build()
+//        retrofit.create(ApiService::class.java)
+//    }
+//}
+//
+//val repositoryModule = module {
+//    single { LocalDataSource(get()) }
+//    single { RemoteDataSource(get()) }
+//    factory { AppExecutors() }
+//    single<ITourismRepository> { TourismRepository(get(), get(), get()) }
+//}
